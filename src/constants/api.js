664eb0e1eb3e5234eeb6ca0aa74973b5a130c301
Anandhu KS <anandhu.ks@bankopen.co>
2024-08-09 11:31:27 +0530

    revert api

--- src/constants/api.js
+++ src/constants/api.js
@@ -2,7 +2,6 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3MmIxNzg1ZGI3NWYxMWM4Y2ZiZDlmNjczNGRiMWQzOSIsIm5iZiI6MTcyMjkyOTczNS4yMTU1ODQsInN1YiI6IjYwYzQ5NTQzOTM4MjhlMDA0MGIxYmFiNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nMRlZiB9iGZ18EGblnRCSkFW1BiRz2gD-CIYgdsnW58'
     }
 };
 
