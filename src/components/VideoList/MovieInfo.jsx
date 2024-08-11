f7748819a91418b0ec8fa9450c37b203c0e2d725
Anandhu KS <anandhu.ks@bankopen.co>
2024-08-11 12:11:56 +0530

    movie info dialog

--- src/components/VideoList/MovieInfo.jsx
+++ src/components/VideoList/MovieInfo.jsx
@@ -1,17 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CORE_URL, API_OPTIONS } from "../../constants/api";
 
 function MovieInfo({ closeDialog, movie }) {
+  const [videoList, setVideoList] = useState(null);
+
+  useEffect(() => {
+    const url = `${CORE_URL}/${movie.id}/videos`;
+    fetch(url, API_OPTIONS)
+      .then((res) => res.json())
+      .then((json) => setVideoList(json.results))
+      .catch((err) => console.error("error:" + err));
+  }, []);
+
+  if (!videoList)
+    return (
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          className="inline w-20 h-20 text-white animate-spin dark:text-gray-600 fill-primary-bg"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+      </div>
+    );
+
+  const mainVideo = videoList[0];
+
   return (
-    <div>
-      <div className="w-full rounded-md p-4 bg-white h-1/2">
+    <div style={{ width: "60vw" }}>
+      <div className="w-full rounded-md p-4 bg-primary-bg shadow">
         <div className="flex justify-end">
           <button className="cursor-pointer" onClick={closeDialog}>
-            <i className="fa fa-close" aria-hidden="true"></i>
+            <i
+              className="fa fa-close text-red-500 fa-lg"
+              aria-hidden="true"
+            ></i>
           </button>
         </div>
-        <div className="mt-4 ">
-          <div className="font-semibold">{movie.original_title}</div>
+
+        <div className="p-4  text-white z-50">
+          <div className="text-4xl">
+            <div className="font-semibold">{movie.original_title}</div>
+          </div>
+          <div className="">{movie.overview}</div>
         </div>
+
+        <iframe
+          className="w-full aspect-video relative"
+          src={`https://www.youtube.com/embed/${mainVideo.key}?autoplay=1&mute=1`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
